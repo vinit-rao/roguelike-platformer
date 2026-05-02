cc5edc76d2542640cc5afb7c0c78a88538b7c4b4
Vinit Rao <virtualvinityt@gmail.com>
2026-05-02 02:35:56 -0400

    working randomized world level select

--- map_generator.gd
+++ map_generator.gd
@@ -9,8 +9,14 @@ extends Node
 @export var horizontal_spacing: float = 100.0
 @export var vertical_spacing: float = 80.0
 
+@export var map_node_scene: PackedScene
+
 # This will hold the raw data of our generated map
 var map_layers: Array = []
+
+func _ready() -> void:
+	generate_map_data()
+	spawn_map_visually()
 
 func generate_map_data() -> void:
 	map_layers.clear()
@@ -49,3 +55,35 @@ func determine_node_type(row: int) -> int:
 	if randf() > 0.8:
 		return 2
 	return 1
+	
+func spawn_map_visually() -> void:
+	for row_index in range(map_layers.size()):
+		var current_row = map_layers[row_index]
+		
+		# trick to make it have a pyramid shape
+		var row_width = (current_row.size() - 1) * horizontal_spacing
+		var start_x = -row_width / 2.0
+		
+		for col_index in range(current_row.size()):
+			var node_data = current_row[col_index]
+			
+			# create new copy of map node
+			var new_node = map_node_scene.instantiate()
+			add_child(new_node)
+			
+			# assign color
+			new_node.node_type = node_data["type"]
+			
+			# set pos
+			var x_pos = start_x + (col_index * horizontal_spacing)
+			var y_pos = -row_index * vertical_spacing
+			new_node.global_position = Vector2(x_pos, y_pos)
+			node_data["physical_node"] = new_node
+
+	# start at start node
+	var start_node = map_layers[0][0]["physical_node"]
+	var player = get_parent().get_node("MapPlayer")
+	
+	if player and GameManager.current_map_node == null:
+		player.current_node = start_node
+		player.global_position = start_node.global_position
