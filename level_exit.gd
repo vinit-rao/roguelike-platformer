c8ca935733dc663ca1baff5174c528aaebec5530
Vinit Rao <virtualvinityt@gmail.com>
2026-05-02 02:24:03 -0400

    setup randomized world select generation

--- level_exit.gd
+++ level_exit.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		print("Level Complete! Returning to Overworld...")
 		body.active_modifier = {} 
-		get_tree().change_scene_to_file("res://OverworldMap.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://OverworldMap.tscn")
--- map_generator.gd
+++ map_generator.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# height is how many rows to reach the boss stage
 @export var map_height: int = 5
 @export var min_nodes_per_layer: int = 2
 @export var max_nodes_per_layer: int = 4
@@ -10,3 +11,41 @@ extends Node
 
 # This will hold the raw data of our generated map
 var map_layers: Array = []
+
+func generate_map_data() -> void:
+	map_layers.clear()
+	
+	for current_row in range(map_height):
+		var layer_nodes: Array = []
+		
+		# node count in group
+		var node_count: int
+		if current_row == 0:
+			node_count = 1 # start node
+		elif current_row == map_height - 1:
+			node_count = 1 # boss node
+		else:
+			# rng for how many branches
+			node_count = randi_range(min_nodes_per_layer, max_nodes_per_layer)
+			
+		for i in range(node_count):
+			var node_data = {
+				"row": current_row,
+				"col": i,
+				"type": determine_node_type(current_row),
+				"next_connections": []
+			}
+			layer_nodes.append(node_data)
+			
+		map_layers.append(layer_nodes)
+
+func determine_node_type(row: int) -> int:
+	if row == 0:
+		return 0 # start
+	if row == map_height - 1:
+		return 3 # boss
+		
+	# For rows in between its rn 20% for shop and 80% for level
+	if randf() > 0.8:
+		return 2
+	return 1
